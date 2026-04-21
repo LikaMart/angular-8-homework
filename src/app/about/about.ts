9ab1ceb38261a78fa76e8dd1c74b8165395f2922
Leka <lmartashvili1@gmail.com>
2026-04-21 21:03:26 +0400

    init commit

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarPipe } from '../star-pipe';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StarPipe],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  text1 = "someText";
+  text2 = "SOmetext";
+  text3 = "someText";
+
+  price= 99.99;
+  percent= 0.01;
+  today = new Date().toDateString();
+}
